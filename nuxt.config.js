ab905614fc235d135f9d50e4d509373263ee789f
Manas Paul <manas@codelogicx.com>
2022-05-07 17:41:16 +0530

    :sparkles: Docerize our project

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  server: {
+    host: process.env.NUXT_HOST,
+    port: process.env.NUXT_PORT,
+  },
   head: {
     title: 'nuxtssrdeploy',
     htmlAttrs: {
