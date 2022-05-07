7986b4119cdf71a7fd9483b1148498a748dc56b7
Manas Paul <manas@codelogicx.com>
2022-05-07 14:38:06 +0530

    :sparkles: Initialize application

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container m-auto">
+    <nuxt-link to="/">Home</nuxt-link> /
+    <nuxt-link to="/about">About</nuxt-link>
+    <h1 class="text-4xl p-10">About</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutUs',
+}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container m-auto">
+    <nuxt-link to="/">Home</nuxt-link> /
+    <nuxt-link to="/about">About</nuxt-link>
+    <Tutorial />
+    <p class="absolute bottom-0 right-20">
+      &copy; {{ new Date().getFullYear() }} by manascode.com
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
